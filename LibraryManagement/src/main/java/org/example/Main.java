e929072cb842961f7626a84176d3aa30e0a2331c
Tejprakash18 <tej22upa.dhyay@gmail.com>
2025-02-22 21:10:06 +0530

    changes

--- LibraryManagement/src/main/java/org/example/Main.java
+++ LibraryManagement/src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
         System.out.println("LLD projects");
+        System.out.println("hey...");
     }
 }
